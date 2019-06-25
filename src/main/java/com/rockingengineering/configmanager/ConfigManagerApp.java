f212d02c71eeac259cb3184a0c2c671f6737c5ac
Naveen Kumar <navuniverse@gmail.com>
2019-06-25 14:25:03 +0530

    Added object mapper for date

--- src/main/java/com/rockingengineering/configmanager/ConfigManagerApp.java
+++ src/main/java/com/rockingengineering/configmanager/ConfigManagerApp.java
@@ -1,13 +1,37 @@
 package com.rockingengineering.configmanager;
 
+import java.text.DateFormat;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @SpringBootApplication
 public class ConfigManagerApp extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigManagerApp.class, args);
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		mapper.setTimeZone(TimeZone.getTimeZone("Asia/Kolkata"));
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
+		mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		mapper.setDateFormat(DateFormat.getDateTimeInstance());
+
+		return mapper;
+
 	}
 }
